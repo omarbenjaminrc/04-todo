18465ad21e65be2c595076b9d4e53306536371bf
Omar Benjamin Ramirez Castillo <omarbenjaminrc@gmail.com>
2022-07-18 20:05:07 -0400

    proyecto terminado

---
+++ src/classes/index.js
@@ -0,0 +1,9 @@
+// este es un archivo para exportar todas las clases aqui para tener un lugar centalizado para importar y tener todo el resto del codigo ordenado
+
+import { Todo } from "./todo.class";
+import { TodoList } from "./todo-list-class";
+
+export {
+    Todo,
+    TodoList
+}
